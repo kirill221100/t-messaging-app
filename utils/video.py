146233f9bf97f95f1f02af88024eec712ccac9ea
kirill221100 <kirulkacool@gmail.com>
2024-04-17 07:00:18 +0700

    video fix

--- utils/video.py
+++ utils/video.py
@@ -11,7 +11,7 @@ async def compress_video(vid: bytes):
     with BytesIO(vid) as video:
         async with aiofiles.tempfile.NamedTemporaryFile('wb', delete=False) as temp:
             await temp.write(video.read())
-    video_path = f'videos/{uuid4()}.mp4'
+    video_path = path.absolute().joinpath(f'videos/{uuid4()}.mp4')
     temp_path = path.absolute().joinpath(temp.name)
     process = (
         FFmpeg().option("y").input(temp_path).output(video_path,  vcodec='libx264', crf='27', preset='veryfast')
